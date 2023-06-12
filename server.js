4ac7931ddab728d96c6131fe1b77d12c6855e974
Jerboa <lotaijo@gmail.com>
2023-06-12 17:43:44 +1200

    add a GET route and POST route

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {
-  temperature: "21",
-  date: "04.06.2023",
-  user_response: "great!",
+  temperature: "",
+  date: "",
+  user_response: "",
 };
 
 // Require Express to run server and routes
@@ -11,32 +11,107 @@ const express = require("express");
 // Start up an instance of app
 const app = express();
 
-const port = 5500;
-app.listen(port, () => {
-  console.log(`The app is listening on port ${port}`);
-});
-
-app.get("/", (req, res) => {
-  res.send(projectData);
-});
-
-req = {
-  temperature: "12",
-  date: "05.06.2023",
-  user_response: "freezing!",
-};
-app.post("/", (req, res) => {
-  res.send("Got a POST request");
-});
-
+/* Dependencies */
+const bodyParser = require("body-parser");
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-// app.use(express.static("website"));
+app.use(express.static("website"));
+const port = 3000;
 
 // Setup Server
+
+const server = app.listen(port, listening);
+function listening() {
+  // console.log(server);
+  console.log(`running on localhost: ${port}`);
+}
+
+// GET route
+app.get("/all", sendData);
+
+function sendData(request, response) {
+  response.send(projectData);
+}
+
+// POST route
+app.post("/add", callBack);
+
+function callBack(req, res) {
+  res.send("POST received");
+}
+
+// POST an animal
+// const data = [];
+const data = [
+  {
+    temperature: "15",
+    date: "10.05.2023",
+    user_response: "wow",
+  },
+  {
+    temperature: "25",
+    date: "01.06.2023",
+    user_response: "yeah",
+  },
+];
+app.post("/add", addData);
+
+function addData(request) {
+  let newData = request.body;
+  let newEntry = {
+    temperature: newData.temperature,
+    date: newData.date,
+    user_response: newData.user_response,
+  };
+  data.push(newEntry);
+  console.log(data);
+}
+
+addData({
+  body: {
+    temperature: "13",
+    date: "06.06.2023",
+    user_response: "amazing!",
+  },
+});
+
+// POST
+// const data = [];
+
+// app.post("/projectData", addData);
+
+// function addData(req, res) {
+//   // res.send("Got a POST request");
+//   data.push(req.body);
+// }
+
+// addData({
+//   body: { temperature: "35", date: "12/06/2023", user_response: "wow!" },
+// });
+//----//
+// express.request = {
+//   temperature: "12",
+//   date: "05.06.2023",
+//   user_response: "freezing!",
+// };
+
+// const data = [];
+// app.post("/", addData);
+
+// function addData(request) {
+//   res.send("Got a POST request");
+
+//   data.push(req.body);
+// }
+// addData({
+//   body: { temperature: "35", date: "12/06/2023", user_response: "wow!" },
+// });
+// console.log(data);
--- website/app.js
+++ website/app.js
@@ -3,3 +3,53 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+let baseURL = `https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${time}&appid=${apiKey}`;
+let apiKey = "85809549041cdefbef629f3153fa24b7";
+
+document.getElementById("generate").addEventListener("click", performAction);
+
+function performAction(e) {
+  const newZip = document.getElementById("zip").value;
+  getWeather(baseURL, newZip, apiKey);
+}
+const getWeather = async (baseURL, user_zipcode, key) => {
+  const res = await fetch(baseURL + user_zipcode + key);
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+/* Function to POST data */
+const postData = async (url = "", data = {}) => {
+  console.log(data);
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+  try {
+    const newData = await response.json();
+    // console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+// Call Function
+postData("/addAnimal", {
+  temperature: "35",
+  date: "12/06/2023",
+  user_response: "wow!",
+});
