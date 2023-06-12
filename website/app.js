5428eac82e4324c9d2e463b69b1632b603230e5e
LOLO <luodairou@Laurens-Mini.hub>
2023-06-13 08:22:14 +1200

    fetching api

--- website/app.js
+++ website/app.js
@@ -4,52 +4,35 @@
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
-let baseURL = `https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${time}&appid=${apiKey}`;
+// variables
 let apiKey = "85809549041cdefbef629f3153fa24b7";
+const newZip = document.getElementById("zip").value;
+let country_code = "840";
+
+// API of Coordinates by zip/post code
+
+// let API_coordinatesByZipcode = `http://api.openweathermap.org/geo/1.0/zip?zip=${newZip},${country_code}&appid=${apiKey}`;
+
+// let baseURL = `https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${time}&appid=${apiKey}`;
 
 document.getElementById("generate").addEventListener("click", performAction);
 
 function performAction(e) {
-  const newZip = document.getElementById("zip").value;
-  getWeather(baseURL, newZip, apiKey);
+  console.log("newZip:", newZip);
+  getWeather(newZip, apiKey);
+  console.log("API_coordinatesByZipcode:", API_coordinatesByZipcode);
 }
-const getWeather = async (baseURL, user_zipcode, key) => {
-  const res = await fetch(baseURL + user_zipcode + key);
+const getWeather = async (newZip, apiKey) => {
+  // const res = await fetch(`${API_coordinatesByZipcode}`);
+  const res = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=85809549041cdefbef629f3153fa24b7"
+  );
   try {
     const data = await res.json();
-    console.log(data);
+    console.log("data:", data);
     return data;
   } catch (error) {
     console.log("error", error);
     // appropriately handle the error
   }
 };
-
-/* Function to POST data */
-const postData = async (url = "", data = {}) => {
-  console.log(data);
-  const response = await fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
-
-  try {
-    const newData = await response.json();
-    // console.log(newData);
-    return newData;
-  } catch (error) {
-    console.log("error", error);
-    // appropriately handle the error
-  }
-};
-
-// Call Function
-postData("/addAnimal", {
-  temperature: "35",
-  date: "12/06/2023",
-  user_response: "wow!",
-});
